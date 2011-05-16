be1839ac233458f7ca93097f062ce3becf927426
bm2011 <maas@eventundsport.de>
2011-05-17 00:23:24 +0200

    Added a user controller and layout link test, added spork, autotest, growl, ...(for tests with rspec)

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ SampleApp::Application.routes.draw do
   match '/about', :to => 'pages#about'
 
   match '/help', :to => 'pages#help'
+  
+  match '/signup', :to => 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
