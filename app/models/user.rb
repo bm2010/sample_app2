1efea062061e697fca2e372e8855efcd51db35b1
bm2011 <maas@eventundsport.de>
2011-07-19 01:44:41 +0200

    Can now destroy users (as admins)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20110517051814
+# Schema version: 20110718215910
 #
 # Table name: users
 #
@@ -10,6 +10,7 @@
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean
 #
 
 class User < ActiveRecord::Base
